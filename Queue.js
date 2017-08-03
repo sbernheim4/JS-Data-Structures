0f91b98a1d108b02b0294dbc9585e1cd98310303
sbernheim4 <samuel.bernheim@mlb.com>
2017-08-03 10:39:07 -0400

    Write Queue.js

---
+++ Queue.js
@@ -0,0 +1,29 @@
+module.exports = class Queue {
+
+	constructor() {
+		this.size = 0;
+		this.q = [];
+	}
+
+	dequeue() {
+		const val = this.q[0];
+
+		this.q.splice(0, 1);
+		this.size -= 1;
+
+		return val;
+	}
+
+	enqueue(val) {
+		this.q.push(val);
+		this.size += 1;
+	}
+
+	isEmpty() {
+		return this.size === 0;
+	}
+
+	size() {
+		return this.size;
+	}
+}

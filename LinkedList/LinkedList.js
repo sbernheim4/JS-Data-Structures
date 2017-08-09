ff90e2c960fe6142c1200c2764caa862331c33f9
sbernheim4 <sambernheim@gmail.com>
2017-08-09 18:00:47 -0400

    Write link after method

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -2,10 +2,35 @@ const Node = require('./Node.js');
 
 module.exports = class LinkedList {
 
-	constructor() {
-		this.head = new Node(-1);
+	constructor(dataVal) {
+		let headDataVal = dataVal || -1;
+
+		this.head = new Node(headDataVal);
 		this.tail = this.head;
-		this.size = 0;
+
+		// If no initial value was passed in, the size of the linked list should be 0
+		// otherwise it should be 1
+		this.size = headDataVal === -1 ? 0 : 1;
+	}
+
+	add(data) {
+		this.linkAfter(data);
+	}
+
+	linkAfter(data) {
+		let newNode = new Node(data);
+
+		if (this.head.next === null) {
+			this.head.next = newNode;
+			newNode.prev = this.head;
+			this.tail = this.head.next;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = this.tail.next;
+		}
+
+		this.size += 1;
 	}
 
 }

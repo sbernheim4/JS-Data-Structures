48acc908b31fd8f2cd62e143546c4cb09026aa4d
sbernheim4 <sambernheim@gmail.com>
2017-08-09 18:10:22 -0400

    Keep the head data value -1

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -3,14 +3,12 @@ const Node = require('./Node.js');
 module.exports = class LinkedList {
 
 	constructor(dataVal) {
-		let headDataVal = dataVal || -1;
-
-		this.head = new Node(headDataVal);
+		this.head = new Node(-1);
 		this.tail = this.head;
 
 		// If no initial value was passed in, the size of the linked list should be 0
 		// otherwise it should be 1
-		this.size = headDataVal === -1 ? 0 : 1;
+		this.size = 0;
 	}
 
 	add(data) {
@@ -21,10 +19,12 @@ module.exports = class LinkedList {
 		let newNode = new Node(data);
 
 		if (this.head.next === null) {
+			// If there are no elements in the list
 			this.head.next = newNode;
 			newNode.prev = this.head;
 			this.tail = this.head.next;
 		} else {
+			// If there is at least 1 element in the list
 			this.tail.next = newNode;
 			newNode.prev = this.tail;
 			this.tail = this.tail.next;

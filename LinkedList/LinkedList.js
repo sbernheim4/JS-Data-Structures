f905fc006fadfad725d84bc5492b0a57fdeeb2a0
sbernheim4 <sambernheim@gmail.com>
2017-08-10 13:59:24 -0400

    Write add method when given an index for LinkedList

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -27,23 +27,59 @@ module.exports = class LinkedList {
 		this.linkAfter(data);
 	}
 
-	linkAfter(data, node) {
+	add(index, data) {
+
+		// Return an error if index < 0
+		if (index < 0 || index > this.size) {
+			return new Error("Index out of Bounds");
+		}
+
+		// If index specified is greater than the size of the list, add the node to
+		// the end of the list
+		if (index === this.size) {
+			this.linkAfter(data)
+		}
+
+		// the new node to be inserted
+		let newNode = new Node(data);
+
+		// Advance curr to the node where the new node will be placed after
+		// add(3, 4) Add a node at index 3 whose data value is 4
+
+		// INDEX: 0     1     2     3     4
+		//    EX: 1 --> 2 --> 3 --> 5 --> 6
+		//                   curr
+		let curr = this.head;
+		for (var i = 0; i < index; i++) {
+			curr = curr.next;
+		}
+
+		// Adjust the pointers
+		newNode.next = curr.next;
+		newNode.prev = curr;
+		curr.next.prev = newNode;
+		curr.next = newNode;
+	}
+
+	linkAfter(data) {
 		// node to be added to the list
 		let newNode = new Node(data);
 
 		if (this.head.next === null) {
-			// If there are no elements in the list...
+			// If there are no elements in the list, add the element after the head
 			this.head.next = newNode;
 			newNode.prev = this.head;
 			this.tail = this.head.next;
 		} else {
-			// If there is at least 1 element in the list...
+			// If there is at least 1 element in the list, add the element after the tail
 			this.tail.next = newNode;
 			newNode.prev = this.tail;
 			this.tail = this.tail.next;
 		}
 
 		this.size += 1;
+
+		return True;
 	}
 
 	print() {

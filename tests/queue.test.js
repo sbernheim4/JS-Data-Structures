399ef38a1b2d1b90eef5b9827c4b02b501d546e4
sbernheim4 <sambernheim@gmail.com>
2017-08-10 20:45:14 -0400

    Clean up tests for queue

--- tests/queue.test.js
+++ tests/queue.test.js
@@ -6,20 +6,36 @@ test('queue is empty', () => {
 	expect(q.size()).toBe(0);
 });
 
-test('dequeue error', () => {
-	expect(q.dequeue().toBeAnError('Queue is empty, cannot dequeue'));
+test('isEmpty is true', () => {
+	expect(q.isEmpty()).toBe(true);
 });
 
-test ('add 1 to q', () => {
+test('dequeue error', () => {
+	expect(() => {
+		q.dequeue();
+	}).toThrow();
+});
+
+test('peek error', () => {
+	expect(() => {
+		q.peek();
+	}).toThrow();
+});
+
+test ('add 1 to queue', () => {
 	q.enqueue(1);
 	expect(q.peek()).toBe(1);
 });
 
-test ('1 should be first', () => {
+test ('peek is 1', () => {
 	q.enqueue(2);
 	expect(q.peek()).toBe(1);
 });
 
 test ('size of queue is 2', () => {
 	expect(q.size()).toBe(2);
+});
+
+test('isEmpty is false', () => {
+	expect(q.isEmpty()).toBe(false);
 });
